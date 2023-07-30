194a028f8fc8853a7c0f224cf4397c9f22762a64
XCODE404 <xcode1101999@gmail.com>
2023-07-30 22:52:03 +0630

    error-handler and constants file

---
+++ app/utils/constants.js
@@ -0,0 +1,34 @@
+exports.STATUS_CODES = {
+    OK: 200,
+    BAD_REQUEST: 400,
+    UN_AUTHORISED: 403,
+    NOT_FOUND: 404,
+    INTERNAL_ERROR: 500,
+}
+
+exports.RESPONSE_MESSAGE = {
+    SUCCESS: "Success",
+    CREATED: "Successfully Created",
+    UPDATED: "Successfully Updated",
+    RETRIVED: "Successfully Retrived",
+    DELETED: "Successfully Deleted",
+    NOT_EXIST: "NOT EXIST",
+    SIGN_IN: "Successfully Sign In",
+    SIGN_UP: "Successfully Sign Un",
+    INVALID_PASSWORD: "Invalid Password",
+    FORGET_PASSWORD: "Forget Password",
+    USER_NOT_FOUND: "User Not Found",
+    ALREADY_EXIST: "Already Exist",
+    VALIDATION_ERROR: "Validation Error",
+    UNAUTHORIZED: "Unauthorized",
+    FORBIDDEN: "Forbidden",
+    NOT_FOUND: "Not Found",
+    BAD_REQUEST: "Bad Request",
+    SERVER_ERROR: "Internal Server Error",
+}
+
+exports.DEFINE = {
+    PAGE : 1,
+    OFFSET : 0,
+    MATCHING_QUERY_LIMIT : 10,
+}
