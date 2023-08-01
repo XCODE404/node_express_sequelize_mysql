769b6699553e1611dbaa5f1f03e1d227c881d82a
XCODE404 <xcode1101999@gmail.com>
2023-08-02 00:37:06 +0630

    miss file for employee-service.js

--- app/services/employee-service.js
+++ app/services/employee-service.js
@@ -1,8 +1,9 @@
 // Import the required modules
-const EmployeeRepository = require("../repository/employee-repository");
-const { CapitalizeFirstLetter, EncodePassword, TodayDate, FormatData, DuplicateData, UserAgent, ComparePassword, MakeToken } = require("../utils");
+const { CapitalizeFirstLetter, EncodePassword, TodayDate, DuplicateData, UserAgent, ComparePassword, MakeToken } = require("../utils");
 const { RESPONSE_MESSAGE } = require("../utils/constants");
 const { NotFoundError, ValidationError } = require("../utils/errors/app-errors");
+const EmployeeRepository = require("../repository/employee-repository");
+const MstRoleService = require("./mst_role-service");
 
 // All Business logic will be here
 class EmployeeService {
@@ -21,8 +22,28 @@ class EmployeeService {
         // make token
         let token = await MakeToken({ employee_id: employee.employee_id, role: employee.mst_role.name });
 
-        return FormatData({ employee: employee, token: token });
+        return { employee: employee, token: token };
     };
+
+    static async initSignUpEmployee(req) {
+        // get admin role id
+        const role = await MstRoleService.getMstRole("init");
+
+        const reqEmployee = {};
+        reqEmployee.role_id = role[0].role_id;
+        reqEmployee.name = CapitalizeFirstLetter("developer 1");
+        reqEmployee.email = "developer1@gmail.com"
+        reqEmployee.password = await EncodePassword("12345678");
+        reqEmployee.date_of_joining = await TodayDate();
+        reqEmployee.created_agent = await UserAgent(req.useragent);
+        reqEmployee.updated_agent = await UserAgent(req.useragent);
+
+        // check exist employee
+        const isExistEmployee = await EmployeeRepository.isExistEmployee(reqEmployee.email);
+        await DuplicateData(isExistEmployee);
+
+        return await EmployeeRepository.createEmployee(reqEmployee);
+    }
 
     static async createEmployee(req) {
         const reqEmployee = req.body.data; //JSON.parse(atob(req.body.data));
@@ -37,28 +58,36 @@ class EmployeeService {
         const isExistEmployee = await EmployeeRepository.isExistEmployee(reqEmployee.email);
         await DuplicateData(isExistEmployee);
 
-        const employee = await EmployeeRepository.createEmployee(reqEmployee);
-        return FormatData(employee); 
+        return await EmployeeRepository.createEmployee(reqEmployee);
     }
 
     static async getEmployee(req) {
-        const employee = await EmployeeRepository.getEmployee(req);
-        return FormatData(employee);
+        return await EmployeeRepository.getEmployee(req);
     }
 
     static async selectedEmployee(req) {
         const { employee_id } = req.params;
-        
-        const employee = await EmployeeRepository.selectedEmployee(employee_id);
-        return FormatData(employee);
+
+        return await EmployeeRepository.selectedEmployee(employee_id);
     }
 
     static async updateEmployee(req) {
+        const { employee_id } = req.params;
+        const reqEmployee = req.body.data; //JSON.parse(atob(req.body.data));
+        
+        const employee = await EmployeeRepository.selectedEmployee(employee_id);
+        if (employee.results === null) throw new NotFoundError(RESPONSE_MESSAGE.USER_NOT_FOUND);
 
+        return await EmployeeRepository.updateEmployee(employee_id, reqEmployee);
     }
 
     static async deleteEmployee(req) {
-        
+        const { employee_id } = req.params;
+
+        const employee = await EmployeeRepository.selectedEmployee(employee_id);
+        if (employee.results === null) throw new NotFoundError(RESPONSE_MESSAGE.USER_NOT_FOUND);
+
+        return await EmployeeRepository.deleteEmployee(employee_id);
     }
 }
 
